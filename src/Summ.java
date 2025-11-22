38c48a6d2e738ac8b4576dac24d6720a0a80bc55
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-11-22 22:10:56 +0530

    LeetCode

---
+++ src/Summ.java
@@ -0,0 +1,20 @@
+class Summ {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+
+    }
+
+    public static void main(String[] args) {
+        int num[] = {2,4,5,6};
+        int target = 9;
+        Summ obj = new Summ();
+
+    }
+}
