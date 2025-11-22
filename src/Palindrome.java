7dadf280554c95b966075c6ce24ec04f66ec8264
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-11-22 22:16:04 +0530

    LeetCode

---
+++ src/Palindrome.java
@@ -0,0 +1,24 @@
+public class Palindrome {
+public boolean isPalindrome(int x) {
+
+    if (x < 0) return false;
+
+    int original = x;
+    int rev = 0;
+    while(x > 0){
+        int num2 = x % 10;
+        x = x/10;
+        rev = rev * 10 + num2  ;
+
+    }
+    return original == rev;
+}
+
+    public static void main(String[] args) {
+        int num = 232;
+        Palindrome obj = new Palindrome();
+        obj.isPalindrome(232);
+
+
+    }
+}
