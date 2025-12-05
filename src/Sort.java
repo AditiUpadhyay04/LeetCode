bbc57bfc8e84c4915d518bb77c63e636c8cc66a4
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-12-05 19:11:50 +0530

    LeetCode

--- src/Sort.java
+++ src/Sort.java
@@ -2,19 +2,24 @@ public class Sort {
     public int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
 
-        int i = 0; // slow pointer
+        int num = 0; // slow pointer
 
-        for (int j = 1; j < nums.length; j++) { // fast pointer
-            if (nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
+        for (int i = 1; i < nums.length; i++) { // fast pointer
+            if (nums[i] != num) {
+                num++;
+                num = nums[i];
             }
         }
-        return i + 1;
+        //   return num + 1;
+        System.out.println(num);
+        return num + 1;
     }
 
+
     public static void main(String[] args) {
+        int[] arr = {1,1,1};
         Sort s1 = new Sort();
+        s1.removeDuplicates(arr);
     }
 }
 
