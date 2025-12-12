162f1f221583a2a1d208242834957d625fac719b
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-12-12 22:56:05 +0530

    LeetCode

--- src/GoodPair.java
+++ src/GoodPair.java
@@ -1,19 +1,19 @@
 public class GoodPair {
     public int numIdenticalPairs(int[] nums) {
         int n = nums.length;
-        int count = 0;
+        int countt = 0;
         for(int i = 0; i < n - 1; i++){
             for(int j = i + 1; j < n; j++){
                 if(nums[i] == nums[j]) {
-                    count++;
+                    countt++;
                 }
 
             }
 
 
         }
-        System.out.println(count);
-        return count;
+        System.out.println(countt);
+        return countt;
 
     }
 
