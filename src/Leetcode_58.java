00389f226d3bf2ec4eba1cd2113f2ce1141e960c
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-12-13 23:47:57 +0530

    LeetCode

---
+++ src/Leetcode_58.java
@@ -0,0 +1,24 @@
+public class Leetcode_58 {
+
+        public int lengthOfLastWord(String s) {
+            s = s.trim();          // trim first
+            int n = s.length();    // then get length
+            int count = 0;
+
+            for (int i = n - 1; i >= 0; i--) {
+                if (s.charAt(i) == ' ')
+                    break;
+                count++;
+            }
+            System.out.println(count);
+            return count;
+        }
+
+    public static void main(String[] args) {
+       String  s = "Hello Moonnnn ";
+       Leetcode_58 L1 = new Leetcode_58();
+       L1.lengthOfLastWord(s);
+    }
+    }
+
+
