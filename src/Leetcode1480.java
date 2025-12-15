f8e666d8e13d3c28727235c26558348e656aff9d
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-12-15 23:33:35 +0530

    LeetCode

--- src/Leetcode1480.java
+++ src/Leetcode1480.java
@@ -1,18 +1,19 @@
 public class Leetcode1480 {
 
-        public int[] runningSum(int[] nums) {
-            for(int i = 1; i < nums.length; i++){
+        public int[] runningSum(int[] nums)
+        {
+            for(int i = 1; i < nums.length; i++)
+            {
                 nums[i] = nums[i] +  nums[i - 1];
             }
-
             return nums;
-
         }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int[] arr = {1,2,3,4};
         Leetcode1480 L1 = new Leetcode1480();
         L1.runningSum(arr);
     }
-    }
+}
 
