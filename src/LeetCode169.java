c0ebadc6c295c8842f8e0d777268ee8f53d1daaf
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-12-16 22:54:37 +0530

    LeetCode

---
+++ src/LeetCode169.java
@@ -0,0 +1,31 @@
+public class LeetCode169 {
+
+        public int majorityElement(int[] nums) {
+            int count = 0;
+            int val = 0;
+
+            for(int i = 0 ; i < nums.length ; i++){
+                if(count == 0){
+                    val = nums[i];
+
+                }
+                if(nums[i] == val)
+                    count++;
+
+                else
+                    count--;
+
+            }
+            System.out.println(val);
+            return val;
+        }
+
+    public static void main(String[] args) {
+            int [] arr = {3,2,3,2,3,2,2};
+        LeetCode169 L1 = new LeetCode169();
+        L1.majorityElement(arr);
+
+    }
+
+}
+
