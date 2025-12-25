d2d95468b3d70af03eb7ad68b65c1b1203c9e21f
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-12-25 23:57:16 +0530

    LeetCode

---
+++ src/Leetcode_13.java
@@ -0,0 +1,35 @@
+public class Leetcode_13 {
+        public int romanToInt(String s) {
+            int sum = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                int curr = value(s.charAt(i));
+
+                if (i + 1 < s.length() && curr < value(s.charAt(i + 1))) {
+                    sum -= curr;
+                } else {
+                    sum += curr;
+                }
+            }
+            return sum;
+        }
+
+        int value(char c) {
+            if (c == 'I') return 1;
+            if (c == 'V') return 5;
+            if (c == 'X') return 10;
+            if (c == 'L') return 50;
+            if (c == 'C') return 100;
+            if (c == 'D') return 500;
+            return 1000; // M
+        }
+
+    public static void main(String[] args) {
+      String s = "III";
+      Leetcode_13 l1 = new Leetcode_13();
+      l1.romanToInt(s);
+
+     }
+}
+
+
