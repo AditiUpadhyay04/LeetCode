f9beedb9d9fbd06b547afaafd74344ce9c230ec2
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-01-08 22:30:48 +0530

    LeetCode

---
+++ src/Leetcode.java
@@ -0,0 +1,21 @@
+public class Leetcode {
+
+        public int largestAltitude(int[] gain) {
+            int altitude = 0;
+            int max = 0;
+
+            for (int i = 0; i < gain.length; i++) {
+                altitude = altitude + gain[i];
+                if (altitude > max) {
+                    max = altitude;
+                }
+            }
+            return max;
+        }
+
+    public static void main(String[] args) {
+
+    }
+    }
+
+
