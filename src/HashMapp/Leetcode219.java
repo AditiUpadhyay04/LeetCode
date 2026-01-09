b19ace86906c6a1bcf09bfd49c7291c40f5822b5
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-01-09 19:06:34 +0530

    LeetCode

---
+++ src/HashMapp/Leetcode219.java
@@ -0,0 +1,24 @@
+package HashMapp;
+
+import java.util.HashMap;
+
+public class Leetcode219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> H1 = new HashMap<>();
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(H1.containsKey(nums[i])){
+                if(Math.abs(i-H1.get(nums[i])) <= k){
+                    return true;
+                }
+            }
+            H1.put(nums[i] , i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
