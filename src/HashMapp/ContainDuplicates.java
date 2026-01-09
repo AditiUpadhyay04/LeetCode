22ff4fd34592c6cb189c8f4b4ac3b0b710a4001b
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-01-09 19:48:48 +0530

    LeetCode

--- src/HashMapp/ContainDuplicates.java
+++ src/HashMapp/ContainDuplicates.java
@@ -1,10 +1,10 @@
 package HashMapp;
-
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class ContainDuplicates {
+    public boolean containsDuplicate(int[] nums) {
 
-        public boolean containsDuplicate(int[] nums) {
             HashMap<Integer,Integer> HM = new HashMap<>();
 
             int n = nums.length;
@@ -21,6 +21,20 @@ public class ContainDuplicates {
 
             return false;
         }
+
+    /*HashSet <Integer> set = new HashSet<>();
+    int n = nums.length;
+
+        for(int i = 0; i < n; i++){
+
+        if(set.contains(nums[i])){
+            return true;
+        }
+        set.add(nums[i]);
+    }
+        return false;
+
+     */
 
     public static void main(String[] args) {
 
