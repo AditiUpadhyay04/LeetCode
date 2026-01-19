95613908f4d73d9e3a131dc6563b0fa65fc5ed31
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-01-19 23:24:18 +0530

    LeetCode

---
+++ src/Leetcode342.java
@@ -0,0 +1,21 @@
+public class Leetcode342 {
+
+        public boolean isPowerOfFour(int n) {
+            if (n <= 0)
+                return false;
+
+            while(n % 4 == 0){
+                n = n/ 4;
+            }
+            if(n == 1)
+                return true;
+            else
+                return false;
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
