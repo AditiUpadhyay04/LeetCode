6651dd9dcbaf2cd7002d6802038bb86523c8a65b
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-01-22 23:52:50 +0530

    LeetCode

---
+++ src/Leetcode53.java
@@ -0,0 +1,26 @@
+public class Leetcode53 {
+
+        public int maxSubArray(int[] nums) {
+
+            int n = nums.length;
+            int max = Integer.MIN_VALUE;
+            int sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+
+                sum = Math.max(sum + nums[i] , nums[i]);
+                max = Math.max(sum,max);
+
+
+                if(sum < 0)
+                    sum = 0;
+            }
+            return max;
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
