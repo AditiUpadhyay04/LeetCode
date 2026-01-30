d4f034c10606e3da6b9b89e80409825ae898313b
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-01-30 19:41:23 +0530

    LeetCode

---
+++ src/Leetcode1343.java
@@ -0,0 +1,38 @@
+public class Leetcode1343 {
+
+        public int numOfSubarrays(int[] arr, int k, int threshold) {
+            int n = arr.length;
+            int i = 0;
+            int j = 0;
+            int sum = 0;
+            int max = 0;
+            int count = 0;
+
+            while(j < n){
+                sum += arr[j];
+
+                if(j - i + 1 < k)
+                {
+                    j++;
+                }
+                else if(j - i + 1 == k)
+                {
+                    max =sum/k;
+                    if(max >= threshold){
+                        count++;
+                    }
+
+                    sum -= arr[i];
+                    i++;
+                    j++;
+                }
+            }
+            return count;
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
