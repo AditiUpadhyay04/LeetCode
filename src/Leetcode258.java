edd3b0871df2f7b8fbeefa3ddb6a0736331877f2
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-01-31 22:55:08 +0530

    LeetCode

---
+++ src/Leetcode258.java
@@ -0,0 +1,24 @@
+public class Leetcode258 {
+        public int addDigits(int num) {
+
+            while(num >= 10){
+                int sum = 0;
+
+                while(num > 0){
+                    sum += num % 10;
+                    num = num/ 10;
+                }
+                num = sum;
+
+            }
+
+            return num;
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
