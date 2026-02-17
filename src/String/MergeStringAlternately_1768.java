4f976f56f3306074bd51f1b028a56233e556cb1b
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-02-17 14:37:10 +0530

    P1768

--- src/String/MergeStringAlternately_1768.java
+++ src/String/MergeStringAlternately_1768.java
@@ -3,11 +3,14 @@ package String;
 public class MergeStringAlternately_1768 {
 
         public String mergeAlternately(String word1, String word2) {
-            int n = word1.length();
+            int n = word1.        length();
             int m = word2.length();
             StringBuilder sb = new StringBuilder();
+
             int i = 0;
+
             int j = 0;
+
             while(i < n || j < m){
                 if(i < n){
                     sb.append(word1.charAt(i));
