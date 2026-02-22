8828aacbdfda6ea957601c8d979ec3fdb47b97f4
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-02-22 19:03:38 +0530

    P1768

--- src/String/MergeStringAlternately_1768.java
+++ src/String/MergeStringAlternately_1768.java
@@ -3,7 +3,7 @@ package String;
 public class MergeStringAlternately_1768 {
 
         public String mergeAlternately(String word1, String word2) {
-            int n = word1.        length();
+            int n = word1.length();
             int m = word2.length();
             StringBuilder sb = new StringBuilder();
 
