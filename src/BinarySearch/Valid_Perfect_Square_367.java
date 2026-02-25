e3783b98ae6bc396ef2a7cba45f4b09fdb864baf
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-02-25 19:01:40 +0530

    P367

---
+++ src/BinarySearch/Valid_Perfect_Square_367.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+
+public class Valid_Perfect_Square_367 {
+
+        public boolean isPerfectSquare(int num) {
+
+            long low = 1;
+            long high = num;
+
+            while(low <= high){
+                long mid = low + (high - low) / 2;
+                if(mid * mid == num){
+                    return true;
+
+                }
+                else if( mid * mid > num){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            return false;
+
+
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
