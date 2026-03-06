c6cb53733dded39e31ad8286b4272b933cb5b2c3
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-03-06 18:55:54 +0530

    P153

--- src/BinarySearch/MINIMUM_ROTATED_ARRAY.java
+++ src/BinarySearch/MINIMUM_ROTATED_ARRAY.java
@@ -1,27 +1,43 @@
 package BinarySearch;
 
 public class MINIMUM_ROTATED_ARRAY {
-
         public int findMin(int[] nums) {
             int n = nums.length;
             int low = 0;
             int high = n - 1;
+            int ans = Integer.MAX_VALUE;
+
+        /*while(low < high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] > nums[high] ){
+                low = mid + 1;
+            }
+            else{
+                high = mid;
+            }
+        }
+            return nums[high];
+            */
 
             while(low < high){
-
                 int mid = low + (high - low) / 2;
-                if(nums[mid] > nums[high] ){
+                if(nums[low] < nums[mid]){
+                    ans = Math.min(ans , nums[low]);
                     low = mid + 1;
                 }
                 else{
-                    high = mid;
+                    ans = Math.min(ans , nums[mid]);
+                    high = mid - 1;
                 }
+
             }
-            return nums[high];
+            return ans;
+
         }
 
     public static void main(String[] args) {
 
     }
 }
+
 
