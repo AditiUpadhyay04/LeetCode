89a45b6f31b4cb07bfdd4fd786b7ec92c2c1c6f1
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-03-11 22:46:58 +0530

    P852

---
+++ src/BinarySearch/Peak_Index_Mountain_Array.java
@@ -0,0 +1,26 @@
+package BinarySearch;
+
+public class Peak_Index_Mountain_Array {
+
+        public int peakIndexInMountainArray(int[] arr) {
+            int n = arr.length;
+            int low = 0;
+            int high = n - 1;
+
+            while(low < high){
+                int mid = low + (high - low) / 2;
+                if(arr[mid] < arr[mid + 1]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid;
+                }
+            }
+            return low;
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
