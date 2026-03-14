b0f900dd49bfd9dcefc96fc50665adec0a89b1d9
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-03-14 21:10:34 +0530

    P392

---
+++ src/String/IsSubsequence_392.java
@@ -0,0 +1,27 @@
+package String;
+
+public class IsSubsequence_392 {
+        public boolean isSubsequence(String s, String t) {
+            int n = s.length();
+            int m = t.length();
+            int i = 0;
+            int j = 0;
+            while (i < n && j < m){
+                if(s.charAt(i) == t.charAt(j)){
+                    i++;
+                    j++;
+                }
+                j++;
+            }
+            if(i == n){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    public static void main(String[] args) {
+
+    }
+}
+
