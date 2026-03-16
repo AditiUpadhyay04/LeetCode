9dbba908bfcb703fc69b1c3e1b2cf8b43a8c7800
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-03-16 22:24:35 +0530

    P206

---
+++ src/LInkedList/ReverseLinkedList.java
@@ -0,0 +1,32 @@
+package LInkedList;
+
+public class ReverseLinkedList_206 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+   /*     public ListNode reverseList(ListNode head) {
+            ListNode temp = head;
+            ListNode prev = null;
+            while(temp != null){
+                ListNode forward = temp.next;
+                temp.next = prev;
+                prev = temp;
+                temp = forward;
+            }
+            return prev;
+
+        }
+
+    */
+    public static void main(String[] args) {
+
+    }
+}
