b497c75b2b5a602748aa458c314332202aa47312
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-03-26 23:14:39 +0530

    P19

---
+++ src/LInkedList/Remove_Nth_Node_From_End_Linked_List.java
@@ -0,0 +1,32 @@
+package LInkedList;
+
+public class Remove_Nth_Node_From_End_Linked_List {
+
+     /*   public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode slow = dummy;
+            ListNode fast = dummy;
+
+            for(int i = 0; i < n; i++){
+                fast = fast.next;
+            }
+
+            while(fast.next != null){
+                slow = slow.next;
+                fast = fast.next;
+
+            }
+            slow.next = slow.next.next;
+
+            return dummy.next;
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+
+      */
+}
+
