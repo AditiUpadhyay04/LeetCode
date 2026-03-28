9872cb1cb4dbd66f3162a0b82ed66dd9b1d6e367
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-03-28 21:34:20 +0530

    P876

---
+++ src/LInkedList/Middle_Of_Linked_List.java
@@ -0,0 +1,23 @@
+package LInkedList;
+
+/*public class Middle_Of_Linked_List {
+
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while(fast != null && fast.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+
+            }
+            return slow;
+
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
+    */
+
