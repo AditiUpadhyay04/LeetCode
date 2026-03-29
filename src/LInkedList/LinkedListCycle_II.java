7b3dc629916ea507b4d89c13a9b94547fa2751a5
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-03-29 22:09:17 +0530

    P142

---
+++ src/LInkedList/LinkedListCycle_II.java
@@ -0,0 +1,32 @@
+package LInkedList;
+
+/*public class LinkedListCycle_II {
+        public ListNode detectCycle(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+
+
+            while(fast != null && fast.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if(slow == fast){
+                    ListNode dummy = head;
+                    while( dummy != slow){
+                        dummy = dummy.next;
+                        slow = slow.next;
+                    }
+                    return dummy;
+
+                }
+            }
+            return null;
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+ */
+
