0cfa3570334a358f427ac4ca461eb32b55c93212
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-03-29 22:08:52 +0530

    P86

---
+++ src/LInkedList/Partition_List.java
@@ -0,0 +1,35 @@
+package LInkedList;
+
+public class Partition_List {
+       /* public ListNode partition(ListNode head, int x) {
+            ListNode dummy1 = new ListNode(0);
+            ListNode dummy2 = new ListNode(0);
+
+            ListNode node = head;
+
+            ListNode temp1 = dummy1;
+            ListNode temp2 = dummy2;
+
+            while(node != null){
+                if(node.val < x){
+                    temp1.next = node;
+                    temp1 = temp1.next;
+                }
+                else{
+                    temp2.next = node;
+                    temp2 = temp2.next;
+                }
+                node = node.next;
+            }
+            temp1.next = dummy2.next;
+            temp2.next = null;
+
+            return dummy1.next;
+        }
+
+        */
+       public static void main(String[] args) {
+
+       }
+    }
+
