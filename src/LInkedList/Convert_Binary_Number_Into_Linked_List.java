34e31761cd406ee35eddb409abee53195016ede2
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-03-30 23:29:56 +0530

    P1290

---
+++ src/LInkedList/Convert_Binary_Number_Into_Linked_List.java
@@ -0,0 +1,32 @@
+package LInkedList;
+
+public class Convert_Binary_Number_Into_Linked_List {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+     /*   public int getDecimalValue(ListNode head) {
+            ListNode temp = head;
+            int num = 0;
+
+            while(temp != null){
+                num = num * 2 + temp.val;
+                temp = temp.next;
+            }
+            return num;
+
+        }
+
+      */
+    public static void main(String[] args) {
+
+    }
+}
+
