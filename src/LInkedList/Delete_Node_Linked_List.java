d057fd914e2ea1a6c6d8fa0acdd2691756ee7b82
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-03-30 23:35:01 +0530

    P237

---
+++ src/LInkedList/Delete_Node_Linked_List.java
@@ -0,0 +1,26 @@
+package LInkedList;
+
+public class Delete_Node_Linked_List {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+
+      /*  public void deleteNode(ListNode node) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+
+
+        }
+
+
+       */
+    public static void main(String[] args) {
+
+    }
+}
+
