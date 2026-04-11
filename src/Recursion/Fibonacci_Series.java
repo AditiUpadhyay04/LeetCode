3c2f32c3bb3b304ac1f93fdccc40e775f1e5d3ac
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-04-11 23:12:00 +0530

    P509

---
+++ src/Recursion/Fibonacci_Series.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Fibonacci_Series {
+
+        public int fib(int n) {
+            if(n == 0 || n == 1) return n;
+
+            return fib(n-1) + fib(n-2);
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
