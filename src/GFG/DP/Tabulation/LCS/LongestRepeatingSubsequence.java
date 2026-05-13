c6801554e23c13f4dee2c910ab3c4915069b4175
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-05-13 21:16:58 +0530

    GFG

---
+++ src/GFG/DP/Tabulation/LCS/LongestRepeatingSubsequence.java
@@ -0,0 +1,28 @@
+package GFG.DP.Tabulation.LCS;
+
+public class LongestRepeatingSubsequence {
+        public int LongestRepeatingSubsequencee(String s) {
+
+            int n = s.length();
+            int m = s.length();
+
+            int[][] t = new int[n + 1][m + 1];
+            for(int i = 1 ; i < n + 1; i++){
+                for(int j = 1; j < m + 1; j++){
+                    if(s.charAt(i - 1) == s.charAt(j - 1) && i != j){
+                        t[i][j] = 1 + t[i - 1][j - 1];
+                    }
+                    else{
+                        t[i][j] = Math.max(t[i - 1][j] , t[i][j - 1]);
+                    }
+                }
+            }
+            return t[n][m];
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
