61b8aaa951e3b70d936b3b683dc6d9efc61813f8
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-05-13 21:18:19 +0530

    GFG

---
+++ src/GFG/DP/Tabulation/LCS/LongestCommonSubsequence.java
@@ -0,0 +1,28 @@
+package GFG.DP.Tabulation.LCS;
+
+public class LongestCommonSubsequence {
+        static int lcs(String s1, String s2) {
+            int n = s1.length();
+            int m = s2.length();
+            int[][] t = new int[n + 1][m + 1];
+
+
+            for(int i = 1; i < n + 1; i++){
+                for(int j = 1; j < m + 1; j++){
+                    if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                        t[i][j] = 1 + t[i - 1][j - 1];
+                    }
+                    else{
+                        t[i][j] = Math.max(t[i - 1][j] , t[i][j - 1]);
+                    }
+
+                }
+
+            }
+            return t[n][m];
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
