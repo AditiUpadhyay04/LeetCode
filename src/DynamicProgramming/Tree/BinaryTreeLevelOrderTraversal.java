9e7164ddaee7d8290d8b3d93207518d7ff322490
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-05-18 22:19:38 +0530

    P102

---
+++ src/DynamicProgramming/Tree/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,48 @@
+package DynamicProgramming.Tree;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversal {
+       class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> ans = new ArrayList<>();
+            Queue<TreeNode> q1 = new LinkedList<>();
+
+            if(root == null) return ans;
+            q1.offer(root);
+
+            while(!q1.isEmpty()){
+                int size = q1.size();
+
+                List<Integer> level = new ArrayList<>();
+                for(int i = 0; i < size; i++){
+                    root = q1.poll();
+                    level.add(root.val);
+                    if(root.left != null) q1.offer(root.left);
+                    if(root.right != null) q1.offer(root.right);
+
+                }
+                ans.add(level);
+            }
+            return ans;
+
+        }
+    }
+}
+//102
