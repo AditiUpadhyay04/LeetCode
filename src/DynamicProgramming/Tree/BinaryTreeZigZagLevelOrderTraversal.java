7e9c27caefd41cb704f4bbc9778469079a35d211
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-05-27 23:27:47 +0530

    P103

---
+++ src/DynamicProgramming/Tree/BinaryTreeZigZagLevelOrderTraversal.java
@@ -0,0 +1,47 @@
+package DynamicProgramming.Tree;
+
+import java.util.*;
+//103
+public class BinaryTreeZigZagLevelOrderTraversal {
+     public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    class Solution {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            List<List<Integer>> ans = new ArrayList<>();
+            Queue<TreeNode> q = new LinkedList<>();
+
+            if(root == null) return ans;
+            q.offer(root);
+            boolean LR = true;
+            while(!q.isEmpty()){
+                int size = q.size();
+                Integer[] list = new Integer[size];
+                for(int i = 0; i < size; i++){
+                    root = q.poll();
+                    int idx = LR ? i : size - 1 - i;
+                    list[idx] = root.val;
+                    if(root.left != null) q.offer(root.left);
+                    if(root.right != null) q.offer(root.right);
+                }
+                LR = !LR;
+                ans.add(Arrays.asList(list));
+            }
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
