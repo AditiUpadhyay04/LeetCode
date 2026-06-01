615a4a20927fd36745e90bdd0c9142d16f6b61a7
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-06-01 23:03:49 +0530

    P98

---
+++ src/DynamicProgramming/Tree/BinarySearchTree/ValidateBST.java
@@ -0,0 +1,42 @@
+package DynamicProgramming.Tree.BinarySearchTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidateBST {
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }     TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    class Solution {
+        List<Integer> ans = new ArrayList<>();
+        public void solve(TreeNode root){
+            if(root == null) return;
+
+            solve(root.left);
+            ans.add(root.val);
+            solve(root.right);
+        }
+        public boolean isValidBST(TreeNode root) {
+            solve(root);
+            for(int i = 0; i < ans.size() - 1; i++){
+                if(ans.get(i) >= ans.get(i + 1)) return false;
+
+
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
