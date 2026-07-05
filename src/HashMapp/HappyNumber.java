a329cb8787bd40caf17f90a8cd2d0f91306dae88
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-07-05 23:38:49 +0530

    P202

---
+++ src/HashMapp/HappyNumber.java
@@ -0,0 +1,32 @@
+package HashMapp;
+
+import java.util.HashSet;
+
+public class HappyNumber {
+        public boolean isHappy(int n) {
+            HashSet<Integer> hs = new HashSet<>();
+
+            while(n != 1){
+                if(hs.contains(n)){
+                    return false;
+                }
+
+                hs.add(n);
+                int sum = 0;
+
+
+                while(n > 0){
+                    int num = n % 10;
+                    sum += num * num;
+                    n = n/10;
+                }
+                n = sum;
+            }
+            return true;
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
