99efb95b1eac500fef3871d125f4de08f8b238ff
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-07-12 23:10:41 +0530

    P455

---
+++ src/Array/AssignCookies.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+        public int findContentChildren(int[] g, int[] s) {
+            Arrays.sort(g);
+            Arrays.sort(s);
+
+            int i = 0;
+            int j = 0;
+
+            while(i < g.length && j < s.length){
+                if(s[j] >= g[i]){
+                    i++;
+                }
+
+                j++;
+
+            }
+            return i;
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
