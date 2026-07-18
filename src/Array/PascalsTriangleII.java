acd0e8c87375c7f35956008628ee910985d833d1
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-07-18 23:15:50 +0530

    P119

---
+++ src/Array/PascalsTriangleII.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangleII {
+
+    public List<Integer> getRow(int rowIndex) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        long val = 1;
+
+        for (int i = 0; i <= rowIndex; i++) {
+            ans.add((int) val);
+
+            val = val * (rowIndex - i) / (i + 1);
+
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
