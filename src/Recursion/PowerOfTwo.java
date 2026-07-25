3a8306665bdf9dcbe6ac5b3a91f11c8df3af7fec
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-07-25 23:51:59 +0530

    P231

---
+++ src/Recursion/PowerOfTwo.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+public class PowerOfTwo {
+        public boolean isPowerOfTwo(int n) {
+            if(n <= 0)
+            {
+
+                return false;
+
+            }
+            while(n % 2 == 0)
+            {
+                n = n / 2 ;
+            }
+            if(n == 1)
+                return true;
+            else
+                return false;
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
